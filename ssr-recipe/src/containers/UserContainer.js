96b4dc77db1851324d95432d45ff4a5e6d163175
tentenacy <tentenacy@gmail.com>
2021-03-29 16:38:23 +0900

    Commit ssr-recipe

---
+++ ssr-recipe/src/containers/UserContainer.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import User from '../components/User';
+import { Preloader, usePreloader } from '../lib/PreloadContext';
+import { getUser } from '../modules/users';
+
+const UserContainer = ({id}) => {
+    const user = useSelector(state => state.users.user);
+    const dispatch = useDispatch();
+
+    usePreloader(() => dispatch(getUser(id))); // 서버 사이드 렌더링을 할 때 API 호출하기
+
+    useEffect(() => {
+        if(user && user.id === parseInt(id, 10)) return;
+        dispatch(getUser(id));
+    }, [dispatch, id, user]);
+
+    if(!user) {
+        return null;
+    }
+    return <User user={user} />;
+};
+
+export default UserContainer;
---
+++ ssr-recipe/src/containers/UsersContainer.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import Users from '../components/Users';
+import { connect } from 'react-redux';
+import { getUsers } from '../modules/users';
+import { Preloader } from '../lib/PreloadContext';
+
+const UsersContainer = ({ users, getUsers }) => {
+    useEffect(() => {
+        if(users) return; // users가 이미 유효하다면 요청하지 않음
+        getUsers();
+    }, [getUsers, users]);
+    return (
+        <>
+            <Users users={users} />
+            <Preloader resolve={getUsers} />
+        </>
+    );
+};
+
+export default connect(
+    state => ({
+        users: state.users.users
+    }),
+    {
+        getUsers
+    }
+)(UsersContainer);
