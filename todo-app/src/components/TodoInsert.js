a80550d1c8369af4f449bb652507dd1c3caa4261
tentenacy <tentenacy@gmail.com>
2021-03-29 16:39:23 +0900

    Commit todo-app

---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,43 @@
+import React, { useState, useCallback, useRef } from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState(''); 
+
+    const inputRef = useRef();
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            inputRef.current.focus();
+
+            // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+            // 이를 방지하기 위해 이 함수를 호출합니다.
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+                ref={inputRef}
+            />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
+    )
+}
+
+export default TodoInsert;
